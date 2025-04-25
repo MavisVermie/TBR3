29ec7884852068a11b5c6981ddad4bdba70bb565
MavisVermie <mavis9982@gmail.com>
2025-04-25 22:03:39 +0300

    Update Navbar.js

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -3,7 +3,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import freecyclelogo from './assets/freecyclelogo.png'
 import freecyclelogo2 from './assets/logo2.png'
-
+//this is a third comment
 import React, { Fragment, useState, useEffect } from "react";
 import { toast } from 'react-toastify';
  
