e111f69d10f31bb9950042b81463c2555b1e8f33
ManarAbu-Irsheid <manarabuirsheid@gmail.com>
2025-05-20 21:40:28 +0300

    mmm

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 const HeroSection=()=>{
     return(
-        <section className="bg-green-600" text-white py-20 px-6 text-center>
+        <section className="bg-green-600 text-white py-20 px-6 text-center">
 <h1>Help us spread goodness</h1>
 <p>Your donation makes a difference</p>
         </section>
