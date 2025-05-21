14bc6c9929fe072f80627f40745860de4cf3fd78
ManarAbu-Irsheid <manarabuirsheid@gmail.com>
2025-05-21 16:44:58 +0300

    mm

--- client/src/pages2/Home.jsx
+++ client/src/pages2/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
 import { Leaf, Shirt, Recycle, Sparkles } from "lucide-react";
 
 export default function HomePage() {
