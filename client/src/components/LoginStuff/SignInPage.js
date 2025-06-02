7a27b98de099f2d8cc13bff4a906e5b80c8511f4
MavisVermie <127209916+MavisVermie@users.noreply.github.com>
2025-06-02 05:45:33 +0300

    Update SignInPage.js

--- client/src/components/LoginStuff/SignInPage.js
+++ client/src/components/LoginStuff/SignInPage.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useNavigate, useEffect } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import logo from "../../assets/TB.png";
 
