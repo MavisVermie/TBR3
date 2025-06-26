1ce3a0d98063a5e9ff24b129f007631c0dc36eb1
MarahYousef01 <gittmarah@gmail.com>
2025-06-26 07:18:19 +0300

    changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,10 +6,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        bannerImg: "url(/public/bg1.gif)",
+        bannerImg: "url(/public/bg2.gif)",
         phoneimg: "url(/public/smbg.gif)",
         tabletimg: "url(/public/md.gif)",
         contimg: "url(/public/paper.jpg)",
+       forarabic: "url(/public/bg3.gif)",
       },
       fontFamily: {
         bitter: ['Bitter', 'serif'],
